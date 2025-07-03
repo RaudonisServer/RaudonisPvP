7a467a68fbd35d9c47290a55f9f397fc1d67800c
TheAnonym3000 <186270664+TheAnonym3000@users.noreply.github.com>
2025-07-03 16:22:00 +0200

    Initial Commit

--- src/main/java/de/raudonis/raudonisPvP/RaudonisPvP.java
+++ src/main/java/de/raudonis/raudonisPvP/RaudonisPvP.java
@@ -1,13 +1,17 @@
 package de.raudonis.raudonisPvP;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class RaudonisPvP extends JavaPlugin {
 
+    public static final Logger LOGGER = LoggerFactory.getLogger("RaudonisPvP");
+
     @Override
     public void onEnable() {
-
-
+        LOGGER.info("Starting RaudonisPvP...");
+        LOGGER.info("Started RaudonisPvP!");
     }
 
     @Override
--- src/main/java/de/raudonis/raudonisPvP/events/PlayerDeath.java
+++ src/main/java/de/raudonis/raudonisPvP/events/PlayerDeath.java
@@ -1,4 +1,20 @@
 package de.raudonis.raudonisPvP.events;
 
-public class PlayerDeath {
+import org.bukkit.*;
+import org.bukkit.block.Vault;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+import java.util.Objects;
+
+public class PlayerDeath implements Listener {
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent e) {
+        Location arena = new Location(Bukkit.getWorlds().getFirst(), 1500, -60, 1500);
+        e.setShouldDropExperience(false);
+        if (Objects.requireNonNull(e.getPlayer().getLastDeathLocation()).distance(arena) < 413) {
+           //TODO
+        }
+    }
 }
